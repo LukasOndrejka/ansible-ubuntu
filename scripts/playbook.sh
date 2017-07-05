9ef3d1dcf65c2a225793e39fe41d0129688db159
lukasondrejka <lukas.ondrejka1@gmail.com>
2017-07-05 14:54:14 +0200

    update

---
+++ scripts/playbook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ../roles/
+cat \
+  <(echo -e "---\n\n- name: Ansible playbook\n  hosts: localhost\n  roles:") \
+  <(for file in *; do echo -e "    # - "${file##*/}; done) \
+  > ../playbook.yml
---
+++ scripts/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIR="~/.ansible"
+REPOSITORY="https://github.com/lukasondrejka/ansible-ubuntu.git"
+
+
+sudo apt-get install -y git python python-dev
+
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA"
+    sudo apt-add-repository ppa:ansible/ansible -y
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    sudo apt-get update
+    sudo apt-get install software-properties-common ansible git python-apt -y
+else
+    echo "Ansible already installed"
+fi
+
+cd ~
+mkdir $DIR
+cd $DIR
+git clone $REPOSITORY
+
+cd $DIR/ansible-ubuntu/scripts
+
+chmod u+x *.sh
+bash ./playbook.sh
+bash ./group_vars.sh
+
+echo ""
+echo "Customize the playbook (ansible-desktop.yml) and the vars (group_vars/all.yml) to suit your needs, then run ansible with:"
+echo "  ansible-playbook playbook.yml  "
+echo ""
